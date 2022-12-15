6d70b6ebee1e7a453cfa2bd731b868bccf841449
Mihail <mohonovproduction@yandex.ru>
2022-12-16 00:27:29 +0300

    remove crunch with token setup

--- src/main.js
+++ src/main.js
@@ -5,11 +5,6 @@ import router from './router'
 import store from './store'
 import components from '@/components/UI/index'
 
-
-
-//Crunch
-localStorage.setItem('user_token', 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJzdWIiOjEsInRva2VuVHlwZSI6MiwiYmxvY2tJZCI6MTQyLCJ3ZWVrIjoxLCJpYXQiOjE2NzEwNDgxOTYsImV4cCI6MTY3OTY4ODE5Nn0.gTHknYr9__xomF7xyil2ySYRcRaI5voQG_sPBbfNlLQ')
-
 const app = createApp(App)
 
 components.forEach(component => app.component(component.name, component))
